0fe0be826e48930a1176b10521ed755fd7adb838
JunBin Huang <junbin.huang@JunBindeMacBook-Pro.local>
2021-03-23 09:41:51 +0800

    fix dynamic database migration

--- dynamicBundle/src/main/java/com/cloud/disk/bundle/dynamic/data/local/LocalDataSource.kt
+++ dynamicBundle/src/main/java/com/cloud/disk/bundle/dynamic/data/local/LocalDataSource.kt
@@ -2,6 +2,8 @@ package com.cloud.disk.bundle.dynamic.data.local
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.cloud.disk.bundle.dynamic.Dynamic
 import com.cloud.disk.bundle.dynamic.db.AppDatabase
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -10,11 +12,18 @@ import javax.inject.Inject
 class LocalDataSource @Inject constructor(
         @ApplicationContext private var mContext: Context) : DataSource {
 
+    private val MIGRATION_1_2 = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("ALTER TABLE dynamic_info RENAME TO dynamic_info_back_up")
+            database.execSQL("CREATE TABLE dynamic_info ( id  INTEGER PRIMARY KEY NOT NULL, content TEXT NOT NULL,date INTEGER NOT NULL)")
+            database.execSQL("INSERT INTO dynamic_info (id, content,date) SELECT id, content,date FROM dynamic_info_back_up")
+        }
+    }
+
     private val db = Room.databaseBuilder(
             mContext,
             AppDatabase::class.java, "dynamic.db"
-    ).build()
-
+    ).addMigrations(MIGRATION_1_2).build()
 
     //判断游标是否为空
     override suspend fun getDynamicListFromCache(): List<Dynamic> {
--- dynamicBundle/src/main/java/com/cloud/disk/bundle/dynamic/db/AppDatabase.kt
+++ dynamicBundle/src/main/java/com/cloud/disk/bundle/dynamic/db/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.cloud.disk.bundle.dynamic.Dynamic
 
-@Database(entities = arrayOf(Dynamic::class), version = 1)
+@Database(entities = arrayOf(Dynamic::class), version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dynamicDao(): DynamicDao
 }
